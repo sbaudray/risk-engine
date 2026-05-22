d9f9dc067f86da841d291b4ee4973176a036169a
sbaudray <22913636+sbaudray@users.noreply.github.com>
2026-05-22 17:51:08 +0200

    init

---
+++ src/main/java/com/bank/risk_engine/RiskEngineApplication.java
@@ -0,0 +1,13 @@
+package com.bank.risk_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiskEngineApplication.class, args);
+	}
+
+}
